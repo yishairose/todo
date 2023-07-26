5c42f74610a39c170800f769722d97a0e81447ed
yishairose <120883255+yishairose@users.noreply.github.com>
2023-07-26 18:10:13 +0300

    Added edit functionality

--- app.js
+++ app.js
@@ -5,29 +5,51 @@ const list = document.querySelector('.list');
 const todoCount = document.querySelector('.todo-count');
 const clearBtn = document.querySelector('.clear')
 const deleteBtn = document.querySelector('.delete-btn')
-const deleteIcon = '<span class="material-symbols-outlined delete-btn">delete</span>';
+const editBtn = document.querySelector('.edit-btn')
+const deleteIcon = '<div><span class="material-symbols-outlined edit-btn">edit</span><span class="material-symbols-outlined delete-btn">delete</span><div>';
 let count;
+let editStatus = false;
+let currentItem;
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    const item = document.createElement('div');
-    item.classList.add('item');
-    item.innerText = input.value;
-    item.innerHTML += deleteIcon;
-    item.classList.add('new-item')
-    list.appendChild(item);
-    input.value = '';
-    count = list.childElementCount;
-    todoCount.innerText = count;
+    if (editStatus === false) {
+        const item = document.createElement('div');
+        item.classList.add('item');
+        const newP = document.createElement('p');
+        newP.innerText = input.value;
+        item.appendChild(newP)
+        item.innerHTML += deleteIcon;
+        item.classList.add('new-item')
+        list.appendChild(item);
+        input.value = '';
+        count = list.childElementCount;
+        todoCount.innerText = count;
+    } else {
+        currentItem.children[0].innerText = input.value;
+        editStatus = false;
+        input.value = '';
+
+    }
 })
 
 list.addEventListener('click', function (e) {
     if (e.target.classList.contains('delete-btn')) {
-        const parent = e.target.parentElement
+        const parent = e.target.parentElement.parentElement
         list.removeChild(parent);
         count = list.childElementCount;
         todoCount.innerText = count;
 
+    }
+})
+
+list.addEventListener('click', function (e) {
+    editStatus = true;
+    if (e.target.classList.contains('edit-btn')) {
+        const parent = e.target.parentElement.parentElement;
+        const todoValue = parent.children[0].innerText;
+        input.value = todoValue;
+        currentItem = parent;
     }
 })
 
@@ -40,4 +62,8 @@ clearBtn.addEventListener('click', function () {
     count = list.childElementCount;
     todoCount.innerText = count;
 })
+
+
+
+
 
